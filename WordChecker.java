39a11b1e98b77689f519e3e7bce40c7341f156a3
mbawndaluscia <kevshortall@gmail.com>
2016-03-20 02:03:26 +0000

    Added files via upload

--- WordChecker.java
+++ WordChecker.java
@@ -1,15 +1,63 @@
 package kevOOP;
 
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-
-public class WordChecker {
-
+public class WordChecker  {
+	static ArrayList<String> dictionary = new ArrayList<String>();
 	public WordChecker(){
+		loadWords();
+	}
+	
+
+		
+	
+	
+	public boolean validWord(String word){
+		if(dictionary.contains(word)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	private void loadWords(){
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader( new FileReader("All Words.csv"));
+		} catch (FileNotFoundException e1) {
+			
+			e1.printStackTrace();
+		}
+		 String word=""; 
+		
+		 try {
+			while((word=br.readLine())!=null){
+				dictionary.add(word);
+				 
+			 }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		 
 		
 	}
 	
-	public boolean validWord(String word){
+	public static void main(String[] args){
+		WordChecker wc=new WordChecker();
 		
-		return true;
+		
+		System.out.println(WordChecker.dictionary.get(3458));
+		System.out.println(wc.validWord(""));
+		System.out.println(wc.validWord("pop"));
+		
+		
+		
+		
+	
 	}
 }
